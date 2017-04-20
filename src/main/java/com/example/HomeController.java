e3e8b2655c6168bae0f36c30a42410fa974bb127
Valeriy Osipov <info@vosipov.com>
2017-04-20 23:33:23 +0300

    Initial commit

---
+++ src/main/java/com/example/HomeController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by info_000 on 20-Apr-17.
+ */
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String hello()
+    {
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/example/SpringMvcExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcExampleApplication.class, args);
+	}
+}
